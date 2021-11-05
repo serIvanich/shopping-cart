1f5c0a7837274e8a98299020c9be1cbdd8b483e9
serIvanich <serivan1108@gmail.com>
2021-11-05 19:15:25 +0200

    done view info in card

--- src/component/cards/Card.jsx
+++ src/component/cards/Card.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import s from './Cards.module.css'
 
 export const Card = ({card}) => {
-
+const {name, category, price} = card
     return (
         <div className={s.cardBox}>
-            {card.name}
-            {card.category}
-            {card.price}
+            <div style={{fontSize: "16px"}}>
+                {category}
+            </div>
+            <div style={{fontSize: "40px"}}>
+                {name}
+            </div>
+            <div className={s.blockWithButton}>
+
+                <div style={{fontSize: "30px"}}>
+                    $<div style={{fontSize: "60px", display: 'inline-block'}}>{price}</div>
+                </div>
+                <button>buy</button>
+            </div>
+
 
         </div>
     )
